e4964a625cd4a6b3cb72dbfb5280c947a2446ba2
Ivan <ivanruzhytskyy@gmail.com>
2023-07-27 14:18:41 +0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -52,3 +52,14 @@ export {deployFTP}
 
 //Віполнение сценария по умолчанию
 gulp.task('default',  dev);
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./publish/**/*")
+    .pipe(deploy())
+});
