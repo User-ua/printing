1d6d48d3c5d6cdc66ef759a4b687c3674170e9b5
Ivan <ivanruzhytskyy@gmail.com>
2023-07-27 14:26:30 +0300

    git

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,6 @@ var deploy      = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("./publish/**/*")
+  return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
